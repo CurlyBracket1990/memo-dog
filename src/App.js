e68a5e2b96da51225cbe92ffb01411f28659a2da
w3bgir1 <akulshyna@gmail.com>
2019-03-04 15:26:10 +0100

    Added landing component and some styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header><h1>Dog memo game</h1></header>
+        <Landing />
       </div>
     );
   }
