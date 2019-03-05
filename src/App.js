d7ad64f88f65a7751769636955b9fb1b124c8f2e
Stefan <stefan_moes@live.nl>
2019-03-05 09:32:25 +0100

    Blabla

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import DogImage from './components/DogImage'
 import Landing from './components/Landing';
 
 class App extends Component {
- render() {
-   return (
-     <Provider store={store}>
-     <div className="App">
-      	<header><h1>Dog memo game</h1></header>
-       
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header><h1>Dog memo game</h1></header>
 
-       <main>
- 	<Landing />
-         <DogImage />
-       </main>
-     </div>
-     </Provider>
-   );
- }
+
+          <main>
+            <Landing />
+            <DogImage />
+          </main>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
